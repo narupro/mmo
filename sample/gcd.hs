91ca68108bd9aed9ef16a8cc8cdba7ed608628eb
narupro <narupro@livedoor.com>
2012-02-17 05:09:06 +0900

    q使ってなかった

--- sample/gcd.hs
+++ sample/gcd.hs
@@ -1,7 +1,7 @@
 module Main(main) where
 
 gcd' a b = (if a >= b then id else flip) f a b
-    where f a b = let (q, r) = a `divMod` b
+    where f a b = let r = a `mod` b
                   in if r == 0 then b else f b r
            
 main = mapM_ (print.uncurry gcd') [(255, 315), (288, 639), (6292, 8580)]
